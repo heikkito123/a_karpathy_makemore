528aaeeb3dc381532910fae0cdf99f0ccf9a86be
heikkito123 <114811711+heikkito123@users.noreply.github.com>
2026-04-29 11:55:50 +0300

    part 6 wip

--- lesson6_nanoGPT.py
+++ lesson6_nanoGPT.py
@@ -4,12 +4,13 @@ from torch.nn import functional as F
 
 # hyperparameters
 batch_size = 32
-block_size = 8
+block_size = 8 # context window
 max_iters = 3000
 eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32
 
 torch.manual_seed(1337)
 
@@ -57,14 +58,20 @@ def estimate_loss():
 
 # simple bigram model
 class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size) # language modeling head
 
     def forward(self, idx, targets=None):
+        B, T = idx.shape
         # idx and targets are both (B, T) tensors of integers
-        logits = self.token_embedding_table(idx) # (B, T, C)
+        token_emb = self.token_embedding_table(idx) # (B, T, C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
+        x = token_emb + pos_emb
+        logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets == None:
             loss = None
@@ -91,7 +98,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
     
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 
 # create a ptrorch optimizer
